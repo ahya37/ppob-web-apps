9f190d286ec2d0931c338973d6d39d301a87e0d5
ahya37 <ahyadev101@gmail.com>
2026-03-18 22:27:23 +0700

    integrasi login

--- test-connection.js
+++ test-connection.js
@@ -8,8 +8,8 @@ const config = {
     user: process.env.DB_USERNAME,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_DATABASE,
-    server: process.env.DB_HOST || 'localhost',
-    port: parseInt(process.env.DB_PORT || '1433'),
+    server: process.env.DB_HOST || '',
+    port: parseInt(process.env.DB_PORT || ''),
     options: {
         encrypt: true,
         trustServerCertificate: true,
